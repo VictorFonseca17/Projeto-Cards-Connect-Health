09ae2561c6a336b206c089597c8fb009d448bb36
Victor Fonseca <vapfonseca@sga.pucminas.br>
2022-10-23 19:30:56 -0300

    Projeto cards contendo dicas sobre Academia e Nutrição

---
+++ json.js
@@ -0,0 +1,77 @@
+"card dicas academia: "[
+    {
+        "id": 1,
+        "nome": "Dica 1",
+        "descrição": "Aumento de carga progressivo te ajuda a obter melhores resultados."
+    },
+
+    {
+        "id": 2,
+        "nome": "Dica 2",
+        "descrição": "Uma boa execução dos exercicios é mais importante do que a carga usada para os resultados."
+    },
+
+    {
+        "id": 3,
+        "nome": "Dica 3",
+        "descrição": "Execicios aerobicos são importantes mas devem ser feitos ao final do treino."
+    },
+
+    {
+        "id": 4,
+        "nome": "Dica 4",
+        "descrição": "Não deixe de descansar. Antes de treinar na academia, é sempre útil ter uma carga mental."
+    },
+
+    {
+        "id": 5,
+        "nome": "Dica 5",
+        "descrição": "Não se esqueça do aquecimento."
+    },
+
+    {
+        "id": 6,
+        "nome": "Dica 6",
+        "descrição": "Evitar treinar o mesmo grupo muscular em dias seguidos."
+    }
+]
+
+
+
+"card dicas nutrição: "[
+    {
+        "id": 1,
+        "nome": "Dica 1",
+        "descrição": "Uma dieta com bons niveis de carboidrato te dara mais energia durante o seu treino."
+    },
+
+    {
+        "id": 2,
+        "nome": "Dica 2",
+        "descrição": "Ingerir 35ml de água por kilo corporal ajuda no seu controle corporal."
+    },
+
+    {
+        "id": 3,
+        "nome": "Dica 3",
+        "descrição": "Carboidratos como pães, arroz e feijão não são inimigos em uma dieta."
+    },
+
+    {
+        "id": 4,
+        "nome": "Dica 4",
+        "descrição": "A ingestão de carboidratos é importante para te dar enrgia no treino."
+    },
+
+    {
+        "id": 5,
+        "nome": "Dica 5",
+        "descrição": "Deve ser ingerido a quantidade correta de prteina para obter bons resultados. "
+    },
+
+    {
+        "id": 6,
+        "nome": "Dica 6",
+        "descrição": "Aumente o consumo de frutas e legumes."
+    }
+]
